569582e6f2c19303edbebf36a051f13edb6b8c62
Pettarn <1225076678@qq.com>
2019-07-02 07:57:33 +0800

    register

--- src/components/page/register.vue
+++ src/components/page/register.vue
@@ -1,6 +1,16 @@
 <template>
+
     <div id="register">
-        
+        <router-link id="register-head" to="/entry" tag="div">
+            登录
+        </router-link>
+        <!-- 用户名，电话号码，密码，收货地址 -->
+        <div id="register-form">
+            <input type="text" placeholder="用户名">
+            <input type="text" placeholder="电话号码">
+            <input type="text" placeholder="密码">
+            <input type="text" placeholder="收货地址">
+        </div>
     </div>
 </template>
 
@@ -11,7 +21,29 @@ export default {
 </script>
 
 <style scoped>
+#register {
+    position: relative;
+    width: 100%;
+}
+#register-head {
+    position: fixed;
+    width: 100%;
+    background-color: rgb(14, 101, 160);
+    padding: 5px;
+    font-size: 2em;
+}
+#register-form {
+    position: relative;
+    width: 100%;
+    top: 55px;
+}
+#register-form>input {
+    height: 30px;
+    width: 100%;
+    font-size: 2em;
+    margin-bottom: 5px;
 
+}
 </style>
 
 
